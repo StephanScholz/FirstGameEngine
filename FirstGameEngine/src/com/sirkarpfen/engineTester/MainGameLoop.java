814a7e8231beb7b8c35a755e6c1b8640075fd1cf
Stephan Scholz <stephan.scholz@gmx.at>
2021-03-10 14:18:59 +0100

    Rendering and moving the cube

--- FirstGameEngine/src/com/sirkarpfen/engineTester/MainGameLoop.java
+++ FirstGameEngine/src/com/sirkarpfen/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package com.sirkarpfen.engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import com.sirkarpfen.entities.Camera;
+import com.sirkarpfen.entities.Entity;
 import com.sirkarpfen.models.RawModel;
 import com.sirkarpfen.models.TexturedModel;
 import com.sirkarpfen.renderEngine.DisplayManager;
@@ -17,40 +20,104 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
-		
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
 		//OpenGL expects vertices to be defined counter clockwise by default
-		float[] vertices = {
-				-0.5f, 0.5f, 0,		//V0
-				-0.5f, -0.5f, 0,	//V1
-				0.5f, -0.5f, 0,		//V2
-				0.5f, 0.5f, 0		//V3
-		};
-		
-		int[] indices = {
-				0,1,3,	//Top left triangle (V0,V1,V3)
-				3,1,2	//Bottom right triangle (V3,V1,V2)
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0,0,	//V0
-				0,1,	//V1
-				1,1,	//V2
-				1,0		//V3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("sample"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
+		Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5),0,0,0,1);
+		
+		Camera camera = new Camera();
+		
 		while(!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
-			
-			//game logic
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 			
--- FirstGameEngine/src/com/sirkarpfen/shaders/StaticShader.java
+++ FirstGameEngine/src/com/sirkarpfen/shaders/StaticShader.java
@@ -2,12 +2,17 @@ package com.sirkarpfen.shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import com.sirkarpfen.entities.Camera;
+import com.sirkarpfen.toolbox.Maths;
+
 public class StaticShader extends ShaderProgram {
 
-	private static final String VERTEX_FILE = "src/com/sirkarpfen/shaders/vertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/com/sirkarpfen/shaders/fragmentShader.txt";
+	private static final String VERTEX_FILE = "FirstGameEngine/src/com/sirkarpfen/shaders/vertexShader.txt";
+	private static final String FRAGMENT_FILE = "FirstGameEngine/src/com/sirkarpfen/shaders/fragmentShader.txt";
 	
 	private int location_transformationMatrix;
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -24,10 +29,21 @@ public class StaticShader extends ShaderProgram {
 	@Override
 	protected void getAllUniformLocations() {
 		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+		location_viewMatrix = super.getUniformLocation("viewMatrix");
 	}
 	
 	public void loadTransformationMatrix(Matrix4f matrix) {
 		super.loadMatrix(location_transformationMatrix, matrix);
+	}
+	
+	public void loadProjectionMatrix(Matrix4f projection) {
+		super.loadMatrix(location_projectionMatrix, projection);
+	}
+	
+	public void loadViewMatrix(Camera camera) {
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		super.loadMatrix(location_viewMatrix, viewMatrix);
 	}
 	
 }
